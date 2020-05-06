8c0cdaad28264477043ee6cb2ded2b88995b88da
rock22635 <rock014124@gmail.com>
2020-05-06 15:08:15 +0800

    Change old Pagination
    Change old Pagination

--- src/components/pages/Productlist.vue
+++ src/components/pages/Productlist.vue
@@ -39,39 +39,6 @@
       </tbody>
     </table>
     <page :pages="pagination" v-on:chgpage="getproducts"/>
-    <nav aria-label="Page navigation example">
-      <ul class="pagination">
-        <li class="page-item" :class="{ 'disabled': !pagination.has_pre }">
-          <a
-            class="page-link"
-            href="#"
-            aria-label="Previous"
-            @click.prevent="getproducts(pagination.current_page - 1)"
-          >
-            <span aria-hidden="true">&laquo;</span>
-          </a>
-        </li>
-        <li
-          class="page-item"
-          v-for="page in pagination.total_pages" :key="page"
-          :class="{ 'active': pagination.current_page == page }"
-        >
-          <a class="page-link" href="#" @click.prevent="getproducts(page)">{{
-            page
-          }}</a>
-        </li>
-        <li class="page-item" :class="{ 'disabled': !pagination.has_next }">
-          <a
-            class="page-link"
-            href="#"
-            aria-label="Next"
-            @click.prevent="getproducts(pagination.current_page + 1)"
-          >
-            <span aria-hidden="true">&raquo;</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
     <div
       class="modal fade"
       id="ProductModal"
